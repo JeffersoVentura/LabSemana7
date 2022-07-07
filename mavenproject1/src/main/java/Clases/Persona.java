754fd2dc7a318cd610a8cd5c45586d5db516898d
JefferVentu <jefferson.ventura@unmsm.edu.pe>
2022-07-07 16:54:41 -0500

    Actualización clase persona

--- mavenproject1/src/main/java/Clases/Persona.java
+++ mavenproject1/src/main/java/Clases/Persona.java
@@ -12,10 +12,23 @@ public class Persona {
     private String nombre;
     private String apellido;
     private String documento;
+
+    public Persona(String nombre, String apellido, String documento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+    }
     
     public void datosPersona (String nombre, String apellido, String documento){
         this.nombre = nombre;
         this.apellido = apellido;
         this.documento = documento;
+        
+    }
+    
+    public void getDatos(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Documento: " + documento);
     }
 }
