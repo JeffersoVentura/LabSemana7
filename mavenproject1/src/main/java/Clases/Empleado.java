05d33870d342721b44a1eafc2a05d6e732808103
JefferVentu <jefferson.ventura@unmsm.edu.pe>
2022-07-07 16:49:30 -0500

    Actualización clase empleado

--- mavenproject1/src/main/java/Clases/Empleado.java
+++ mavenproject1/src/main/java/Clases/Empleado.java
@@ -11,6 +11,22 @@ package Clases;
 public class Empleado {
     private String codigo;
     private float sueldo;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(float sueldo) {
+        this.sueldo = sueldo;
+    }
     
     
 }
